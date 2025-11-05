1c8a57d790c3151e58d19d0a145869431e1b0af4
Yakesh Kumar Sivakumar <y.sivakumar@perficient.com>
2025-11-05 08:29:06 +0530

    update code

--- src/com/Test.java
+++ src/com/Test.java
@@ -6,9 +6,6 @@ import java.util.UUID;
 public class Test {
 
     public static void main(String[] args) {
-//        System.out.println(UUID.randomUUID().toString().substring(0,8));
-//
-//        Random random = new Random();
-//        random.
+        System.out.println("Welcome to Java Spring boot");
     }
 }
